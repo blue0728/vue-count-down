f1155d0700143a2b126b0de3ae547ec41019106a
Serge <serge@haimi.com>
2016-08-01 15:04:43 +0800

    update

--- src/vue-count-down.js
+++ src/vue-count-down.js
@@ -49,11 +49,7 @@
 					h = h < 10 ? '0' + h : h;
 					m = m < 10 ? '0' + m : m;
 					s = s < 10 ? '0' + s : s;
-					if (h == 0) {
-						result = m + ':' + s;
-					} else {
-						result = h + ':' + m + ':' + s;
-					}
+					result = h + ':' + m + ':' + s;
 					return result
 				}
 
